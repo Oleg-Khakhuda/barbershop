c75f1fcc79b466f85a1d6b9dca4c021a265ecddd
Oleg-Khakhuda <79002829+Oleg-Khakhuda@users.noreply.github.com>
2022-10-21 18:51:17 +0000

    Deploying to gh-pages from @ Oleg-Khakhuda/barbershop@15a43bd6f4081c53eab31afd78182740705b8308 🚀

---
+++ index.d13ffb2b.js
@@ -0,0 +1,2 @@
+var menu=function(){var e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]");e.addEventListener("click",(function(){var n="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!n),t.classList.toggle("is-open")}))};menu();
+//# sourceMappingURL=index.d13ffb2b.js.map
